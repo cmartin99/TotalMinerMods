4c2a18855d60f29f1252fd7299d55587e468bee8
Craig Martin <studioforge@slingshot.co.nz>
2018-01-12 14:35:26 +1300

    SimpleGui mod added

---
+++ SimpleGui/ModPlugin.cs
@@ -0,0 +1,78 @@
+using StudioForge.TotalMiner;
+using StudioForge.TotalMiner.API;
+
+namespace SimpleGui
+{
+    public class ModPlugin : ITMPlugin
+    {
+        #region Fields
+
+        ITMGame game;
+        Block lastBlockID;
+
+        #endregion
+
+        #region ITMPlugin
+
+        public void PlayerJoined(ITMPlayer player)
+        {
+        }
+
+        public void PlayerLeft(ITMPlayer player)
+        {
+        }
+
+        public void WorldSaved(int version)
+        {
+        }
+
+        public bool HandleInput(ITMPlayer player)
+        {
+            return false;
+        }
+
+        #endregion
+
+        #region Initialization
+
+        public void Initialize(ITMPluginManager mgr, string path)
+        {
+        }
+
+        public void InitializeGame(ITMGame game)
+        {
+            this.game = game;
+        }
+
+        #endregion
+
+        #region Update
+
+        public void Update()
+        {
+        }
+
+        public void Update(ITMPlayer player)
+        {
+            var blockID = (Block)game.World.Map.GetBlockID(player.SwingTarget);
+            if (blockID != lastBlockID)
+            {
+                lastBlockID = blockID;
+                if (blockID == Block.ColorBlack)
+                {
+                    game.OpenPauseMenu(new SimpleScreen(game, player), player);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Draw
+
+        public void Draw(ITMPlayer player, ITMPlayer virtualPlayer)
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ SimpleGui/TMPluginProvider.cs
@@ -0,0 +1,27 @@
+﻿using StudioForge.TotalMiner.API;
+
+namespace SimpleGui
+{
+    class TMPluginProvider : ITMPluginProvider
+    {
+        public ITMPlugin GetPlugin()
+        {
+            return new ModPlugin();
+        }
+
+        public ITMPluginArcade GetPluginArcade()
+        {
+            return null;
+        }
+
+        public ITMPluginBlocks GetPluginBlocks()
+        {
+            return null;
+        }
+
+        public ITMPluginGUI GetPluginGUI()
+        {
+            return null;
+        }
+    }
+}
