fc34b145ada6cf17e60a21075404217d496a6485
Craig Martin <studioforge@slingshot.co.nz>
2018-01-26 20:52:25 +1300

    robotron

---
+++ TotalRoboArcade/TMPluginProvider.cs
@@ -0,0 +1,27 @@
+using StudioForge.TotalMiner.API;
+
+namespace TotalRobo
+{
+    public class TMPluginProvider : ITMPluginProvider
+    {
+        public ITMPlugin GetPlugin()
+        {
+            return new TotalRoboMod();
+        }
+
+        public ITMPluginBlocks GetPluginBlocks()
+        {
+            return null;
+        }
+
+        public ITMPluginArcade GetPluginArcade()
+        {
+            return new TotalRoboPlugin();
+        }
+
+        public ITMPluginGUI GetPluginGUI()
+        {
+            return null;
+        }
+    }
+}
