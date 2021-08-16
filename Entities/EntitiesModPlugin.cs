c39f9036d3d7c6a5470c4c6cc9a1ad8f57b4b577
cmartin99 <studioforge@slingshot.co.nz>
2021-08-16 22:09:04 +1200

    mods

--- Entities/EntitiesModPlugin.cs
+++ Entities/EntitiesModPlugin.cs
@@ -1,11 +1,16 @@
 using StudioForge.Engine.Integration;
+using StudioForge.BlockWorld;
 using StudioForge.TotalMiner;
 using StudioForge.TotalMiner.API;
+using Microsoft.Xna.Framework;
 
 namespace EntitiesMod
 {
     class EntitiesModPlugin : ITMPlugin
     {
+        Entity boat;
+        ITMGame game;
+
         #region ITMPlugin
 
         void ITMPlugin.UnloadMod()
@@ -18,14 +23,19 @@ namespace EntitiesMod
 
         void ITMPlugin.PlayerJoined(ITMPlayer player)
         {
+            var b = new Entity();
+            if (game.World.EntityManager.AddEntity("entities", "spaceshipcockpit", b) == ErrorCode.Success)
+                boat = b;
         }
 
         void ITMPlugin.PlayerLeft(ITMPlayer player)
         {
+            game.AddNotification($"Player {player.Gamer.Gamertag} has left");
         }
 
         void ITMPlugin.InitializeGame(ITMGame game)
         {
+            this.game = game;
         }
 
         void ITMPlugin.Draw(ITMPlayer player, ITMPlayer virtualPlayer)
@@ -37,8 +47,25 @@ namespace EntitiesMod
             return false;
         }
 
+        int frame;
+
         void ITMPlugin.Update(ITMPlayer player)
         {
+            if ((++frame % 480) == 0)
+                game.AddNotification($"Player {player.Gamer.Gamertag} is updating");
+
+            if (boat != null)
+            {
+                //boat.Position = player.EyePosition;
+                var m = Matrix.Invert(player.ViewMatrix);
+                boat.Position.X = m.M41;
+                boat.Position.Y = m.M42;
+                boat.Position.Z = m.M43;
+                boat.ViewDirection = new Vector3(m.M31, m.M32, m.M33);
+                boat.ViewDirection = player.ViewDirection;
+                boat.DrawRotY = MathHelper.PiOver2;
+                boat.DrawOffY = 0;// -3.5f;
+            }
         }
 
         void ITMPlugin.Update()
